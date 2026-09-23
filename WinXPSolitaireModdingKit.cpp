326e67fb902f9a30546fbf3f7a111b28ed9eed82
kotorifan <kotorifan05@gmail.com>
2026-09-23 05:55:37 +0200

    trying to make a gui

--- WinXPSolitaireModdingKit.cpp
+++ WinXPSolitaireModdingKit.cpp
@@ -1,14 +1,72 @@
-#include <iostream>
-#include <fstream>
 #include <cstdint>
-#include <vector>
+#include <cstdlib>
 #include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <vector>
 
-#include <imgui.h>
+#include <windows.h>
+
+#define APPLICATION_NAME "WinXPSolitaireModdingKit";
 
 static std::string s_bak_path;
 
+namespace UI
+{
+	static constexpr uint32_t ok_button = 1002;
+	static enum : uint32_t {
+		APPLY_PATCH_INVERT_BG_COLOR = 1,
+		WIN_CLOSE = 2
+	};
+	LRESULT CALLBACK WndProc(HWND hwnd,
+		UINT msg,
+		WPARAM wp,
+		LPARAM lp)
+	{
+		switch (msg)
+		{
+		case WM_CREATE:
+			CreateWindowW(
+				L"STATIC",
+				L"Invert background color",
+				WS_VISIBLE | WS_CHILD,
+				20, 20, 100, 20, hwnd,
+				nullptr,
+				nullptr,
+				nullptr
+			);
+			CreateWindowW(
+				L"BUTTON",
+				L"Apply",
+				WS_VISIBLE | WS_CHILD,
+				20, 20, 100, 30, hwnd,
+				(HMENU)APPLY_PATCH_INVERT_BG_COLOR,
+				nullptr,
+				nullptr
+			);
+			CreateWindowW(
+				L"BUTTON",
+				L"Quit",
+				WS_VISIBLE | WS_CHILD,
+				20, 20, 100, 50, hwnd,
+				(HMENU)WIN_CLOSE,
+				nullptr,
+				nullptr
+			);
+		}
+	}
+}
+	
+	void startup()
+	{
 
+	}
+
+	void update()
+	{
+
+	}
+}
 
 namespace IO
 {
